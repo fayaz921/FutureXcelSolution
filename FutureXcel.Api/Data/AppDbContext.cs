2e47d0147bde809bb897217f06928d4441fd2401
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-03 23:17:21 +0500

    Create AppDbContext.cs

---
+++ FutureXcel.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FutureXcel.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FutureXcel.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Testitems> Testitems { get; set; }
+    }
+}
